19b07302300e18985545641462bdb385e76b8c2b
mgsantos89 <107523374+mgsantos89@users.noreply.github.com>
2022-09-15 08:20:40 -0300

    Create variables.tf

---
+++ network/variables.tf
@@ -0,0 +1,17 @@
+variable "cidr_vpc" {
+    description = "CIDR block para a VPC"
+    type = string
+}
+
+variable "cidr_subnet" {
+    description = "CIDR block para a subnet"
+    type = string
+}
+
+variable "enviroment" {
+    description = "Ambiente onde o recurso sera utilizado"
+    type = string
+
+}
+
+
